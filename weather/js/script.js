ea5ddb976704f49ff41857c0352b03e1b64b05b8
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-06-17 09:38:45 +0900

    [Finish] learn Ajax

--- weather/js/script.js
+++ weather/js/script.js
@@ -1,7 +1,31 @@
+const API_KEY = "531453a9c9694c6a28044d0d8633e18b";
+
 $(function(){
-  $('.menu-trigger').on('click', function(event){
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
+  $('#btn').on('click', function() {
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      //// 通信制高
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      // 湿度
+      $('#humidity').text(data.main.humidity);
+      // 風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('umg').attr("alt", data.weather[0].main);
+    }).fail(function (data){
+      //// 通信失敗
+      alert('通信に失敗しました。');
+    });
   });
 });
